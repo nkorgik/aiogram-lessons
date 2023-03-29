901ae6ccbe8fd2de460f1028d4c1fb6e06f6b344
nkorgik <nkorgik639@gmail.com>
2023-03-30 00:17:28 +0800

    optimized only positive for O(n+k)

--- python_algorithms/python_sorting/count_sort.py
+++ python_algorithms/python_sorting/count_sort.py
@@ -1,16 +1,21 @@
-def counting_sort(arr):
+# The complexity of counting sort is O(n + k), where n is the length of the input array and k is the range of the input
+# (i.e., the difference between the maximum and minimum values).
+# In general, counting sort are useful when you have a large input array with a small range or a small number of digits.
+
+def counting_positive_sort(arr):
     """This function is for positive only"""
     max_value = max(arr)
-    print(max_value)
+    min_value = min(arr)
     
-    count = [0] * (max_value + 1)
+    count = [0] * (max_value - min_value + 1)
     
     for x in arr:
-        count[x] += 1
+        count[x-min_value] += 1
     
     sorted_arr = []
-    for i in range(max_value + 1):
-        sorted_arr.extend([i] * count[i])
+    for i in range(len(count)):
+        for j in range(count[i]):
+            sorted_arr.append(i+min_value)
         
     return sorted_arr
 
@@ -27,6 +32,7 @@ def counting_sort(arr):
     for num in shifted_arr:
         count[num] += 1
 
+    # TODO: should be optimized for complexity O(n+k), think about that =)
     sorted_arr = []
     for i in range(len(count)):
         for j in range(count[i]):
@@ -35,10 +41,9 @@ def counting_sort(arr):
     return sorted_arr
 
 
-
 if __name__ == "__main__":
     arr = [3, 2, 1, 99, 37]
-    result = counting_sort(arr)
+    result = counting_positive_sort(arr)
     print('POSITIVE: ', result, sep='\n')
 
     my_list = [5, -3, 8, -1, 0, -2, 4]
