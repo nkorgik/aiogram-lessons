4437a59ac25cbafd5a5a73526d1a5b65b5a39fe4
nkorgik <nkorgik639@gmail.com>
2023-03-30 00:00:29 +0800

    added negative counting sort

--- python_algorithms/python_sorting/count_sort.py
+++ python_algorithms/python_sorting/count_sort.py
@@ -15,9 +15,33 @@ def counting_sort(arr):
     return sorted_arr
 
 
+def counting_sort(arr):
+    """This if for negative counting sort"""
+    shift = abs(min(arr))
+    shifted_arr = [x + shift for x in arr]
+
+    max_val = max(shifted_arr)
+
+    count = [0] * (max_val + 1)
+
+    for num in shifted_arr:
+        count[num] += 1
+
+    sorted_arr = []
+    for i in range(len(count)):
+        for j in range(count[i]):
+            sorted_arr.append(i - shift)
+
+    return sorted_arr
+
+
+
 if __name__ == "__main__":
     arr = [3, 2, 1, 99, 37]
     result = counting_sort(arr)
-    print(result)
+    print('POSITIVE: ', result, sep='\n')
 
+    my_list = [5, -3, 8, -1, 0, -2, 4]
+    sorted_list = counting_sort(my_list)
+    print('NEGATIVE: ', sorted_list, sep='\n')
     
